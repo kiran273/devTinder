55f80238e4c2aebdd2001e3f13e4b3874000179c
gkkumar <gkkumar7416@gmail.com>
2025-07-24 16:41:09 +0530

    exploring get,post,put,delete

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
 
+//This will only handle GET call to /user
+app.get("/user", function (req, res) {
+  res.send({
+    firstName: "K",
+    lastName: "K",
+  });
+});
 
-app.use(function(req,res){
-  res.send("Hello from the server,ok")
-})
+app.post("/user", function (req, res) {
+  res.send("Data sent");
+});
+
+app.put("/user", function (req, res) {
+  res.send("Data updated");
+});
+
+app.use("/user", function (req, res) {
+  res.send("Hello Testing");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Data deleted");
+});
+
+//This will match all HTTP method API calls to /test
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
